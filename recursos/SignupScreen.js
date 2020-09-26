1b9a089c82bc67bffa4103a514026c9d9877a68c
Norazuniga21 <norazugu2106@gmail.com>
2020-09-26 00:27:36 -0500

    agregandoHijo

--- recursos/SignupScreen.js
+++ recursos/SignupScreen.js
@@ -9,7 +9,6 @@ import {
 } from "react-native";
 import AwesomeAlert from "react-native-awesome-alerts";
 import { firebase } from "./utils/firebase";
-import styles from "./styles/stylesSignupScreen";
 
 const SignupScreen = (props) => {
   const [name, setName] = useState("");
