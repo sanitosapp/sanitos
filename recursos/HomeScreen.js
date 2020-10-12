26a3d2ee779b1ae48167e9a92f046bf6776b35c9
Dnniz <dannuzp@gmail.com>
2020-10-12 12:28:04 -0500

    Implementación de Tabs

--- recursos/HomeScreen.js
+++ recursos/HomeScreen.js
@@ -1,92 +1,127 @@
-import React from 'react'
-import { Alert, Picker, TextInput, TouchableWithoutFeedback, ScrollView, Image, View, Text, StatusBar, StyleSheet, TouchableOpacity, LayoutAnimation, AsyncStorage, Modal, Button } from 'react-native'
-import DatePicker from 'react-native-datepicker'
-import { MaterialIcons } from '@expo/vector-icons'
+import React from "react";
+import {
+  Alert,
+  Picker,
+  TextInput,
+  TouchableWithoutFeedback,
+  ScrollView,
+  Image,
+  View,
+  Text,
+  StatusBar,
+  StyleSheet,
+  TouchableOpacity,
+  LayoutAnimation,
+  AsyncStorage,
+  Modal,
+  Button,
+} from "react-native";
+import DatePicker from "react-native-datepicker";
+import { MaterialIcons } from "@expo/vector-icons";
 
-
-import * as firebase from 'firebase'
-
+import * as firebase from "firebase";
 
 //VISTA HOME PRINCIPAL
 export default class HomeScreen extends React.Component {
-
   static navigationOptions = {
-    headerShown: false
-  }
-
-
-  state = {
-    name: ""
-  }
-
-  state = {
-    escolaridade: ""
+    headerShown: false,
   };
 
   state = {
-    sangre: ""
+    name: "",
   };
 
   state = {
-    data: ""
+    escolaridade: "",
+  };
+
+  state = {
+    sangre: "",
+  };
+
+  state = {
+    data: "",
   };
 
   changeName(name) {
-    this.setState({ name })
+    this.setState({ name });
   }
 
   changeDate = (valor) => {
     this.setState({
-      data: valor
-    })
-  }
+      data: valor,
+    });
+  };
 
   buttonPressed() {
     const arrayDataNino = [];
-    if (this.state.name && this.state.escolaridade && this.state.sangre && this.state.data) {
+    if (
+      this.state.name &&
+      this.state.escolaridade &&
+      this.state.sangre &&
+      this.state.data
+    ) {
       const dataNino = {
         name: this.state.name,
         escolaridade: this.state.escolaridade,
         sangre: this.state.sangre,
-        data: this.state.data
-      }
+        data: this.state.data,
+      };
       arrayDataNino.push(dataNino);
       try {
-        AsyncStorage.getItem('database_agregarnino0').then((value) => {
+        AsyncStorage.getItem("database_agregarnino0").then((value) => {
           if (value !== null) {
             const d = JSON.parse(value);
-            d.push(dataNino)
-            AsyncStorage.setItem('database_agregarnino0', JSON.stringify(d)).then(() => {
-              this.modalHandler()
-            })
+            d.push(dataNino);
+            AsyncStorage.setItem(
+              "database_agregarnino0",
+              JSON.stringify(d)
+            ).then(() => {
+              this.modalHandler();
+            });
           } else {
-            AsyncStorage.setItem('database_agregarnino0', JSON.stringify(arrayDataNino)).then(() => {
-              this.modalHandler()
-            })
+            AsyncStorage.setItem(
+              "database_agregarnino0",
+              JSON.stringify(arrayDataNino)
+            ).then(() => {
+              this.modalHandler();
+            });
           }
-        })
+        });
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
     } else {
-      Alert.alert('Falta completar un campo')
+      Alert.alert("Falta completar un campo");
     }
   }
 
   state = {
-    isVisible: false
-  }
+    isVisible: false,
+  };
+
   goToChart = () => {
-    this.props.navigation.navigate('ChildChart');
-  }
+    let parametro = {
+      ctipoChart: "Altura",
+    };
+    this.props.navigation.navigate("ChildChart", parametro);
+  };
+
+  goToChartPeso = () => {
+    let parametro = {
+      ctipoChart: "Peso",
+    };
+    this.props.navigation.navigate("ChildChart", parametro);
+  };
+
   modalHandler = () => {
-    this.setState({ isVisible: !this.state.isVisible })
-  }
+    this.setState({ isVisible: !this.state.isVisible });
+  };
 
   state = {
     email: "",
-    displayName: ""
-  }
+    displayName: "",
+  };
 
   componentDidMount() {
     const { email, displayName } = firebase.auth().currentUser;
@@ -99,68 +134,73 @@ export default class HomeScreen extends React.Component {
   };
 
   constructor() {
-    super()
+    super();
     this.state = {
-      Nino: ""
-    }
+      Nino: "",
+    };
     try {
-      AsyncStorage.getItem('database_agregarnino0').then((value) => {
+      AsyncStorage.getItem("database_agregarnino0").then((value) => {
         this.setState({
-          Nino: JSON.parse(value)
-        })
-      })
+          Nino: JSON.parse(value),
+        });
+      });
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
   }
   parseData() {
     if (this.state.Nino) {
       return this.state.Nino.map((dataNino, i) => {
         return (
-          <View
-            style={styles.infoCard}
-            key={i}>
-
+          <View style={styles.infoCard} key={i}>
             <View>
               <Text
-                style={{ fontSize: 16, textAlign: 'center', backgroundColor: '#1D96A3', padding: 6, color: '#fff', textTransform: 'uppercase' }}
-              >{dataNino.name} </Text>
+                style={{
+                  fontSize: 16,
+                  textAlign: "center",
+                  backgroundColor: "#1D96A3",
+                  padding: 6,
+                  color: "#fff",
+                  textTransform: "uppercase",
+                }}
+              >
+                {dataNino.name}{" "}
+              </Text>
             </View>
 
-            <View
-              style={{ flexDirection: 'row' }}
-            >
+            <View style={{ flexDirection: "row" }}>
               <View>
                 <Image
-                  source={require('../recursos/imagenes/logoSanitos.png')}
+                  source={require("../recursos/imagenes/logoSanitos.png")}
                   style={{ width: 70, height: 70 }}
                 />
               </View>
-              <View
-                style={{ padding: 10 }}
-              >
+              <View style={{ padding: 10 }}>
                 <Text>{dataNino.escolaridade} </Text>
                 <Text>{dataNino.sangre} </Text>
                 <Text>{dataNino.data} </Text>
-
               </View>
-
-
             </View>
             <View>
               <TouchableOpacity
-                onPress={() => this.props.navigation.navigate('Nino')}
+                onPress={() => this.props.navigation.navigate("Nino")}
               >
                 <Text
-                  style={{ textAlign: 'center', padding: 6, fontSize: 16, color: '#C4C4C4' }}
-                > + Presiona aqui para ver mas  </Text>
+                  style={{
+                    textAlign: "center",
+                    padding: 6,
+                    fontSize: 16,
+                    color: "#C4C4C4",
+                  }}
+                >
+                  {" "}
+                  + Presiona aqui para ver mas{" "}
+                </Text>
               </TouchableOpacity>
-
-
             </View>
           </View>
-        )
-      })
+        );
+      });
     }
   }
 
@@ -169,131 +209,134 @@ export default class HomeScreen extends React.Component {
     LayoutAnimation.easeInEaseOut();
 
     return (
-
-
-
-
-      <ScrollView
-        style={styles.container}
-      >
-        <StatusBar barStyle='light-content' ></StatusBar>
-
-
+      <ScrollView style={styles.container}>
+        <StatusBar barStyle="light-content"></StatusBar>
 
         <Text style={{ marginTop: 60, left: 30, fontSize: 16 }}>
-          Bienvenida {this.state.email} !{'\n'}
+          Bienvenida {this.state.email} !{"\n"}
           Estamos felices de verte por aquí
-          </Text>
-        <View style={styles.containerCards}>
-          {this.parseData()}
-        </View>
+        </Text>
+        <View style={styles.containerCards}>{this.parseData()}</View>
 
         <View style={styles.infoCard}>
-          <TouchableOpacity
-            onPress={this.goToChart}
-          >
+          <TouchableOpacity onPress={this.goToChart}>
             <Text
-              style={{ textAlign: 'center', padding: 20, fontSize: 16, color: '#C4C4C4' }}
-            > Ver Reporte </Text>
+              style={{
+                textAlign: "center",
+                padding: 20,
+                fontSize: 16,
+                color: "#C4C4C4",
+              }}
+            >
+              {" "}
+              Ver Reporte Estatura{" "}
+            </Text>
           </TouchableOpacity>
         </View>
 
         <View style={styles.infoCard}>
-          <TouchableOpacity
-            onPress={this.modalHandler}
-          >
+          <TouchableOpacity onPress={this.goToChartPeso}>
             <Text
-              style={{ textAlign: 'center', padding: 20, fontSize: 16, color: '#C4C4C4' }}
-            > +  Agregue los datos de su niño/niña </Text>
+              style={{
+                textAlign: "center",
+                padding: 20,
+                fontSize: 16,
+                color: "#C4C4C4",
+              }}
+            >
+              {" "}
+              Ver Reporte Peso
+            </Text>
           </TouchableOpacity>
         </View>
 
+        <View style={styles.infoCard}>
+          <TouchableOpacity onPress={this.modalHandler}>
+            <Text
+              style={{
+                textAlign: "center",
+                padding: 20,
+                fontSize: 16,
+                color: "#C4C4C4",
+              }}
+            >
+              {" "}
+              + Agregue los datos de su niño/niña{" "}
+            </Text>
+          </TouchableOpacity>
+        </View>
 
-        <Modal
-          visible={false}
-          transparent={true}
-          animationType='fade'
-        >
+        <Modal visible={false} transparent={true} animationType="fade">
           <TouchableOpacity
             onPress={() => this.modalHandler()}
-            style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
           >
             <TouchableWithoutFeedback>
-
               <View
-                style={{ height: '80%', width: '80%', backgroundColor: 'grey', padding: 10, borderRadius: 4 }}
+                style={{
+                  height: "80%",
+                  width: "80%",
+                  backgroundColor: "grey",
+                  padding: 10,
+                  borderRadius: 4,
+                }}
               >
                 <MaterialIcons
-                  name='close'
+                  name="close"
                   size={24}
                   onPress={() => this.modalHandler()}
-                >
-                </MaterialIcons>
+                ></MaterialIcons>
 
                 <View style={styles.form}>
-
                   <View>
-                    <Text
-                      style={styles.title1}
-                    >Agregar niña/a</Text>
+                    <Text style={styles.title1}>Agregar niña/a</Text>
                   </View>
 
                   <View>
-
                     <TextInput
                       style={styles.input}
-                      placeholder='Nombre'
-                      autoCapitalize='none'
+                      placeholder="Nombre"
+                      autoCapitalize="none"
                       onChangeText={(name) => this.changeName(name)}
                       value={this.state.name}
                     ></TextInput>
                   </View>
 
-
-
                   <View>
-
                     <Picker
                       style={styles.pickerComponent}
                       selectedValue={this.state.escolaridade}
-                      onValueChange={
-                        (itemValor, itemIndex) =>
-                          this.setState({
-                            escolaridade: itemValor
-                          })
+                      onValueChange={(itemValor, itemIndex) =>
+                        this.setState({
+                          escolaridade: itemValor,
+                        })
                       }
-
                     >
-                      <Picker.Item label='Sexo' value='' />
-                      <Picker.Item label='Niña' value='Niña' />
-                      <Picker.Item label='Niño' value='Niño' />
-
+                      <Picker.Item label="Sexo" value="" />
+                      <Picker.Item label="Niña" value="Niña" />
+                      <Picker.Item label="Niño" value="Niño" />
                     </Picker>
                   </View>
 
                   <View>
-
                     <Picker
                       style={styles.pickerComponent}
                       selectedValue={this.state.sangre}
-                      onValueChange={
-                        (itemValor, itemIndex) =>
-                          this.setState({
-                            sangre: itemValor
-                          })
+                      onValueChange={(itemValor, itemIndex) =>
+                        this.setState({
+                          sangre: itemValor,
+                        })
                       }
                     >
-                      <Picker.Item label='Tipo de sangre' value='' />
-                      <Picker.Item label='A positivo' value='A positivo' />
-                      <Picker.Item label='A negativo' value='A negativo' />
-                      <Picker.Item label='B positivo' value='B positivo' />
-                      <Picker.Item label='B negativo' value='B negativo' />
-                      <Picker.Item label='O negativo' value='O negativo' />
-                      <Picker.Item label='O negativo' value='O negativo' />
-                      <Picker.Item label='AB positivo' value='AB positivo' />
-                      <Picker.Item label='AB negativo' value='AB negativo' />
-
-
+                      <Picker.Item label="Tipo de sangre" value="" />
+                      <Picker.Item label="A positivo" value="A positivo" />
+                      <Picker.Item label="A negativo" value="A negativo" />
+                      <Picker.Item label="B positivo" value="B positivo" />
+                      <Picker.Item label="B negativo" value="B negativo" />
+                      <Picker.Item label="O negativo" value="O negativo" />
+                      <Picker.Item label="O negativo" value="O negativo" />
+                      <Picker.Item label="AB positivo" value="AB positivo" />
+                      <Picker.Item label="AB negativo" value="AB negativo" />
                     </Picker>
                   </View>
 
@@ -306,41 +349,21 @@ export default class HomeScreen extends React.Component {
                     />
                   </View>
 
-
-
-
-
                   <TouchableOpacity
                     style={styles.button}
                     onPress={() => this.buttonPressed()}
                   >
-                    <Text
-                      style={{ color: '#ffffff', fontWeight: '500' }}
-                    >Agregar</Text>
+                    <Text style={{ color: "#ffffff", fontWeight: "500" }}>
+                      Agregar
+                    </Text>
                   </TouchableOpacity>
-                  <View>
-
-                  </View>
-
-
-
-
-
-
-
+                  <View></View>
                 </View>
               </View>
-
-
             </TouchableWithoutFeedback>
           </TouchableOpacity>
-        </Modal >
-
-      </ScrollView >
-
-
-
-
+        </Modal>
+      </ScrollView>
     );
   }
 }
@@ -352,7 +375,7 @@ const styles = StyleSheet.create({
   infoCard: {
     marginBottom: 24,
     borderWidth: 1,
-    borderColor: '#05A4AC',
+    borderColor: "#05A4AC",
     borderRadius: 4,
     width: 300,
     left: 30,
@@ -361,12 +384,10 @@ const styles = StyleSheet.create({
     marginTop: 30,
   },
   button: {
-
-    backgroundColor: '#E9446A',
+    backgroundColor: "#E9446A",
     borderRadius: 4,
 
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center",
   },
-
 });
--- recursos/chart/ChildChartScreen.js
+++ recursos/chart/ChildChartScreen.js
@@ -1,106 +1,199 @@
-import React from 'react';
-import {
-    StyleSheet,
-    View,
-    Button
-} from 'react-native';
-import HighchartsReactNative from '@highcharts/highcharts-react-native';
+import React from "react";
+import { StyleSheet, View, Button } from "react-native";
+import { Grid, Col, Input, Tab, Tabs, TabHeading, Text } from "native-base";
+import HighchartsReactNative from "@highcharts/highcharts-react-native";
 
 class Screen extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            chartOptions: {
-                title: {
-                    text: 'Solar Employment Growth by Sector, 2010-2016'
-                },
+    this.state = {
+      initialPage: 1,
+      tabactivo: 1,
+      chartOptions: {
+        title: {
+          text: "Solar Employment Growth by Sector, 2010-2016",
+        },
 
-                subtitle: {
-                    text: 'Source: thesolarfoundation.com'
-                },
+        subtitle: {
+          text: "Source: thesolarfoundation.com",
+        },
 
-                yAxis: {
-                    title: {
-                        text: 'Number of Employees'
-                    }
-                },
+        yAxis: {
+          title: {
+            text: "Number of Employees",
+          },
+        },
 
-                xAxis: {
-                    accessibility: {
-                        rangeDescription: 'Range: 2010 to 2017'
-                    }
-                },
-                chart: {
-                    zoomType: 'x'
-                },
+        xAxis: {
+          accessibility: {
+            rangeDescription: "Range: 2010 to 2017",
+          },
+        },
+        chart: {
+          zoomType: "x",
+        },
+        legend: {
+          layout: "vertical",
+          align: "right",
+          verticalAlign: "middle",
+        },
+
+        plotOptions: {
+          series: {
+            label: {
+              connectorAllowed: false,
+            },
+            pointStart: 2010,
+          },
+        },
+        series: [
+          {
+            name: "Installation",
+            data: [
+              43934,
+              52503,
+              57177,
+              69658,
+              97031,
+              119931,
+              137133,
+              154175,
+              43934,
+              52503,
+              57177,
+              69658,
+              97031,
+              119931,
+              137133,
+              154175,
+              43934,
+              52503,
+              57177,
+              69658,
+              97031,
+              119931,
+              137133,
+              154175,
+            ],
+          },
+          {
+            name: "Manufacturing",
+            data: [24916, 24064, 29742, null, null, 30282, 38121, 40434],
+          },
+          {
+            name: "Sales & Distribution",
+            data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387],
+          },
+          {
+            name: "Project Development",
+            data: [null, null, 7988, 12169, 17310.5, 22452, 34400, 34227],
+          },
+          {
+            name: "Other",
+            data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111],
+          },
+        ],
+
+        responsive: {
+          rules: [
+            {
+              condition: {
+                maxWidth: 500,
+              },
+              chartOptions: {
                 legend: {
-                    layout: 'vertical',
-                    align: 'right',
-                    verticalAlign: 'middle'
+                  layout: "horizontal",
+                  align: "center",
+                  verticalAlign: "bottom",
                 },
+              },
+            },
+          ],
+        },
+      },
+    };
+  }
 
-                plotOptions: {
-                    series: {
-                        label: {
-                            connectorAllowed: false
-                        },
-                        pointStart: 2010
-                    }
-                },
-                series: [{
-                    name: 'Installation',
-                    data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-                }, {
-                    name: 'Manufacturing',
-                    data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-                }, {
-                    name: 'Sales & Distribution',
-                    data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-                }, {
-                    name: 'Project Development',
-                    data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-                }, {
-                    name: 'Other',
-                    data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-                }],
+  componentDidMount() {
+    const { navigation } = this.props;
+    const ctipoChart = navigation.getParam("ctipoChart", "NO-ID");
+    console.log("TEST:params:", ctipoChart);
+    if (ctipoChart) {
+      switch (ctipoChart) {
+        case "Altura":
+          this.setState({ initialPage: 1, tabactivo: 1 });
+          break;
+        case "Peso":
+          this.setState({ initialPage: 0, tabactivo: 0 });
+          break;
+      }
+    }
+  }
+  componentDidUpdate() {}
 
-                responsive: {
-                    rules: [{
-                        condition: {
-                            maxWidth: 500
-                        },
-                        chartOptions: {
-                            legend: {
-                                layout: 'horizontal',
-                                align: 'center',
-                                verticalAlign: 'bottom'
-                            }
-                        }
-                    }]
-                }
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Tabs
+          locked
+          tabContainerStyle={{ height: 40 }}
+          tabBarUnderlineStyle={{
+            backgroundColor: "black",
+            height: 5,
+          }}
+          initialPage={this.state.initialPage}
+          page={this.state.tabactivo}
+        >
+          <Tab
+            heading={
+              <TabHeading style={{ backgroundColor: "#A4D4DB" }}>
+                <Text
+                  style={{ fontSize: 15, color: "black" }}
+                  onPress={() => {
+                    this.setState({ tabactivo: 0 });
+                  }}
+                >
+                  Peso
+                </Text>
+              </TabHeading>
             }
-        };
-    }
-
-    render() {
-        return (
+          >
+            <View></View>
+          </Tab>
+          <Tab
+            heading={
+              <TabHeading style={{ backgroundColor: "#A4D4DB" }}>
+                <Text
+                  style={{ fontSize: 15, color: "black" }}
+                  onPress={() => {
+                    this.setState({ tabactivo: 1 });
+                  }}
+                >
+                  Estatura
+                </Text>
+              </TabHeading>
+            }
+          >
             <View style={styles.container}>
-                <HighchartsReactNative
-                    styles={styles.container}
-                    options={this.state.chartOptions}
-                />
+              <HighchartsReactNative
+                styles={styles.container}
+                options={this.state.chartOptions}
+              />
             </View>
-        );
-    }
+          </Tab>
+        </Tabs>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        height: 200,
-        backgroundColor: '#fff',
-        justifyContent: 'center',
-        flex: 1
-    }
+  container: {
+    height: 200,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    flex: 1,
+  },
 });
 export default Screen;
