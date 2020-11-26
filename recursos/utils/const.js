3403fa6c9f978b151bbb161dbfd5ecc583f94c79
DiegoHerrera <diego.herrera@itsight.pe>
2020-11-26 00:37:31 -0500

    androidClientId by env

--- recursos/utils/const.js
+++ recursos/utils/const.js
@@ -1,5 +1,7 @@
 const enviroment = 'DEV';
-const androidClientId = "891797980558-gh899km8h1nlr5mi8kkovitt7urg2k78.apps.googleusercontent.com";
+const androidClientId = enviroment == 'DEV' ?
+  "891797980558-6gosaull8ttk5v6scnfqrul41f76qp0e.apps.googleusercontent.com"
+  : "891797980558-gh899km8h1nlr5mi8kkovitt7urg2k78.apps.googleusercontent.com";
 const vaccines = () => [
   {
     dose: "Única dosis",
